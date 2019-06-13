d2728727051e050eedcfad86d0b002470e529a8d
mcarneirobug <mcarneiro@sga.pucminas.br>
2019-06-13 18:48:14 -0300

    Exibição conteúdo db no html

--- noticias.js
+++ noticias.js
@@ -1,2 +1,20 @@
 
+    module.exports = function(app) {
+        
+        app.get('/noticias', function(req, res) {
+
+            var mysql = require('mysql');
+
+            var connection = mysql.createConnection({
+                host: 'localhost',
+                user: 'root',
+                password: '1234',
+                database: 'portal_noticias'
+            });
+
+            connection.query('select * from noticias', function(error, result) {
+                res.render("noticias/noticias", {noticias : result}); //poderemos recuperar os dados do banco através de indices no ejs
+            });
+        });
+    };
     
