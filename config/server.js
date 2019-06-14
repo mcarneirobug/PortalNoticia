d9531e5c1fd392e9f62cb63cd114d958f653f948
mcarneirobug <mcarneiro@sga.pucminas.br>
2019-06-14 15:22:54 -0300

    Validação do form com express validator

--- config/server.js
+++ config/server.js
@@ -2,7 +2,8 @@
     var express = require('express');
     var consign = require('consign');
     var bodyParser = require('body-parser');
-
+    var expressValidator = require('express-validator');
+    
     var app = express();
     app.set('views', './app/views');
 
@@ -12,9 +13,11 @@
      */
     app.set('view engine', 'ejs');
 
-    //pasar o body-parser
-    app.use(bodyParser.urlencoded({extended: true}));
+    // os middlers tratão o request
 
+    //passar o body-parser
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(expressValidator());
     /**
      * com o consign nós incluímos todos os 
      * modulos fazendo o require de routes automaticamente
